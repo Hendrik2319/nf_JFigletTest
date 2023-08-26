14d137e4cf843f5022c554faa3a85cfd6a973d9b
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-08-26 09:00:54 +0200

    warning suppressed

--- src/main/java/net/schwarzbaer/java/test/jfiglet/Main.java
+++ src/main/java/net/schwarzbaer/java/test/jfiglet/Main.java
@@ -10,6 +10,7 @@ public class Main {
         System.out.println(generateAsciiArt("Hello world!"));
     }
 
+    @SuppressWarnings("SameParameterValue")
     private static String generateAsciiArt(String str) {
         try {
             return FigletFont.convertOneLine(str);
